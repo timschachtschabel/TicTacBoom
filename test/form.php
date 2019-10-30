244c163d78893ffb56354432116294570ed1bd29
Tim Schachtschabel <timschachtschabel14@gmail.com>
2019-10-30 15:20:08 +0100

    Onur CSS added

--- test/form.php
+++ test/form.php
@@ -1,25 +1,56 @@
 <?php
 include 'components/head.php';
 ?>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</head>
-<body>
+   
 
-<form>
+
+
+<div class="container">
+    <div class="dataform">
+        
+
+
+        <form action="game.php" method="get">
+  <legend>Instellingen</legend>
   <div class="form-group">
-    <label for="exampleInputEmail1">Speler 1</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Naam">
+    <label for="exampleFormControlSelect1">Categorie</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      
+      <option>Topografie</option>
+      <option>Dieren</option>
+
+    </select>
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="exampleFormControlSelect1">Aanstal spelers</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+    </select>
+<br>
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
+    <input type="text" class="form-control"name="fname[]" placeholder="Naam">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <div class="form-group row">
+  <label for="example-number-input"  name="aantalTijd" min="1"  placeholder="Tijd" class="col-2 col-form-label" min="2">Tijd in minuten</label>
+  <div class="col-10">
+    <input class="form-control" type="number" value="42" id="example-number-input">
+    <br>
+  <input type="submit">
+</div>
+  
 </form>
-
+    </div>    
+</div>
 
 
 <?php
