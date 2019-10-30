6ca43d9e4c1880a25d39b899fe1e0b41b49801aa
Tim Schachtschabel <timschachtschabel14@gmail.com>
2019-10-30 10:32:02 +0100

    Added new css file

--- test/components/head.php
+++ test/components/head.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" href="test.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="onur.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+
+    <title>TicTacBoom</title>
 </head>
 <body>
