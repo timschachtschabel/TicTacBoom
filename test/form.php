9468a47a068c67c16299369d6375aadca730bc61
Onur <o.temelli@hotmail.com>
2019-10-31 10:57:03 +0100

    css opmaak

--- test/form.php
+++ test/form.php
@@ -36,11 +36,11 @@ include 'components/head.php';
   </div>
   
   <div class="form-group row">
-  <label for="example-number-input"  name="aantalTijd" placeholder="Tijd" class="col-2 col-form-label" min="2">Tijd in minuten</label>
+  <label for="example-number-input"  name="aantalTijd" placeholder="Tijd" class="col-2 col-form-label" >Tijd in minuten</label>
   <div class="col-10">
     <input class="form-control" type="number" name="aantalTijd" id="example-number-input">
     <br>
-  <input type="submit">
+    <button type="submit" class="btn btn-primary" >Starten!</button>
 </div>
   
 </form>
