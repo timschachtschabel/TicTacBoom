89c18235eb2ef96c618b363b2e2039c37fbc3c4f
Tim Schachtschabel <timschachtschabel14@gmail.com>
2019-11-01 14:10:10 +0100

    Form page styling

--- test/form.php
+++ test/form.php
@@ -18,16 +18,6 @@ require_once 'components/database.php';
         </select>
       </div>
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Aanstal spelers</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-          <option>6</option>
-        </select>
-        <br>
 
         <!-- <input type="text" class="form-control" name="fname[]" placeholder="Naam">
         <input type="text" class="form-control" name="fname[]" placeholder="Naam">
@@ -49,7 +39,7 @@ require_once 'components/database.php';
 
       <div class="form-group row">
 
-        <?php if ($_GET['playername'] == 'admin') { ?>
+        
 
           <label for="example-number-input" name="aantalTijd" placeholder="Tijd" class="col-2 col-form-label" min="2">Tijd in minuten</label>
           <div class="col-10">
@@ -57,7 +47,7 @@ require_once 'components/database.php';
             <br>
             <input type="submit">
           </div>
-        <?php } ?>
+ 
 
     </form>
 
@@ -71,19 +61,20 @@ require_once 'components/database.php';
       if (mysqli_query($conn, $sql)) {
         // echo "New record created successfully";
       } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        // echo "Error: " . $sql . "<br>" . mysqli_error($conn);
       }
       ?>
 
       <p id="playpername"></p>
 
     </div>
-
-    <form action="index.php" method="get">
-      <input type="submit" value="Terug naar start" class="btn btn-warning" name="backtostart">
-      <input type="hidden" name="currentuser" value="<?php echo $_GET['playername'] ?>">
-    </form>
   </div>
+  <div class="backtostart">
+    <form action="index.php" method="get">
+        <input type="submit" value="Terug naar start" class="btn btn-warning" name="backtostart">
+        <input type="hidden" name="currentuser" value="<?php echo $_GET['playername'] ?>">
+    </form>
+  </div>  
 </div>
 
 <script type="text/javascript">
